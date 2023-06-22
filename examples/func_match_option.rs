2524fd6a332d68299e1867ce34d97e9af091351f
dudoof <email@example.com>
2023-06-22 10:08:06 +0200

    add todo

--- examples/func_match_option.rs
+++ examples/func_match_option.rs
@@ -1,3 +1,12 @@
+// cd ~
+// cargo new test_result_option_waiter
+// cd test_result_option_waiter
+// mkdir examples
+// cp src/main.rs examples/func_match_option.rs
+// cargo build --example func_match_option
+// cargo run --example func_match_option
+
+
 fn main() {
     println!("Hello, example!");
 }
