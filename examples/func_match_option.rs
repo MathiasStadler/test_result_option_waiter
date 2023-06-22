635cc0b1523e5138e3e8bace085ad632cf96e84e
dudoof <email@example.com>
2023-06-22 10:16:37 +0200

    function result

--- examples/func_match_option.rs
+++ examples/func_match_option.rs
@@ -6,7 +6,23 @@
 // cargo build --example func_match_option
 // cargo run --example func_match_option
 
+// from here
+// https://blog.logrocket.com/ditching-try-catch-and-null-checks-in-rust/
+
+fn  get_waiter_comment  (  tip_percentage  :  u32  )   ->   Option  <  String  >   { 
+    if  tip_percentage  <=   20   { 
+        None 
+    }   else   { 
+        Some  (  "Das ist ein großzügiger Tipp!"  .  to_string  ()) 
+    } 
+} 
 
 fn main() {
     println!("Hello, example!");
+
+    let tip = 21;
+    match get_waiter_comment  (  tip  )   { 
+        Some  (  comment  )   =>  println!( "Comment => {}", comment  ),
+        None   =>  println!("None = null") 
+      }
 }
