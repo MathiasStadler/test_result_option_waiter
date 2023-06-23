b3095eac1da0c0b93214a8bf3b9d294894d22bb1
dudoof <email@example.com>
2023-06-23 10:22:45 +0200

    add result_return_option

--- examples/optionvsresult.rs
+++ examples/optionvsresult.rs
@@ -2,15 +2,31 @@
 //https://levelup.gitconnected.com/rust-option-vs-result-when-to-use-what-e73e82612cb0
 
 fn main() {
-    println!("{:?}", prog_return());
+    println!("{:?}", prog_return_string());
     println!("{:?}", prog());
+
+    let result_return_option: Option<f64> = return_option(1.0, 0.0);
+    println!("result_return_option -> {:?}", result_return_option);
+
+    match result_return_option {
+        Some(x) => println!("Result of option is :{}", x),
+        None => println!("Cannot divide by 0"),
+    }
 }
 
-fn prog_return() -> String {
+fn prog_return_string() -> String {
     // println!("{}","Hallo prog");
-    return String::from("Hallo Prog return ");
+    return String::from("Prog return String ");
 }
 
 fn prog() -> String {
     String::from("Hello Prog")
+}
+
+fn return_option(numerator: f64, denominator: f64) -> Option<f64> {
+    if denominator == 0.0 {
+        None
+    } else {
+        Some(numerator / denominator)
+    }
 }
